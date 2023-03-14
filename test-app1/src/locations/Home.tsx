ba697db1effca138d05907168c8c870df3238b62
Juho Sillanpää <juho.sillanpaa@hiq.fi>
2023-03-14 12:53:43 +0200

    updates

--- test-app1/src/locations/Home.tsx
+++ test-app1/src/locations/Home.tsx
@@ -1,34 +1,57 @@
-import React from 'react';
-import { Paragraph, Note, Heading, Grid, GridItem } from '@contentful/f36-components';
-import { HomeExtensionSDK } from '@contentful/app-sdk';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import React, { useCallback } from "react";
+import { useCMA, useSDK } from "@contentful/react-apps-toolkit";
+import { useAsync } from "react-async-hook";
+import { Flex, Text } from "@contentful/f36-components";
+import tokens from "@contentful/f36-tokens";
+import Stats from "../components/stats/Stats";
+import Members from "../components/members/Members";
 
-const Home = () => {
-  const sdk = useSDK<HomeExtensionSDK>();
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
+export const Home = () => {
+  const sdk = useSDK();
+  const cma = useCMA();
+  const getSpace = useCallback(async () => {
+    return await cma.space.get({});
+  }, [cma]);
+
+  const { result } = useAsync(getSpace, []);
 
   return (
-    <Grid style={{width: '100%'}}
-    columns='1fr 1fr'
-    columnGap='spacingM'
-    rowGap='spacingM'
-    >
-      <GridItem area='span 1 / span 2' style={{backgroundColor: 'lightblue'}}>
-        <Heading>Welcome back Patrick! </Heading>
-      </GridItem>
-      <GridItem style={{backgroundColor: 'red'}}>
-        elements
-      </GridItem>
-      <GridItem style={{backgroundColor: 'red'}}>
-        hall of fame
-      </GridItem>
-
-    </Grid>
-  )
+    <Flex flexDirection="column" alignItems="center" fullWidth>
+      <Flex
+        justifyContent="center"
+        padding="spacing3Xl"
+        fullWidth
+        style={{ backgroundColor: tokens.gray700 }}
+      >
+        <Flex flexDirection="column" gap="spacingXl" style={{ width: "900px" }}>
+          <Text
+            fontColor="colorWhite"
+            fontSize="fontSize4Xl"
+            fontWeight="fontWeightDemiBold"
+          >
+            👋 Welcome {sdk.user.firstName}! Have a good one!
+          </Text>
+          {result && (
+            <Text
+              fontColor="colorWhite"
+              fontSize="fontSizeXl"
+              fontWeight="fontWeightDemiBold"
+            >
+              Space: {result.name}
+            </Text>
+          )}
+        </Flex>
+      </Flex>
+      <Flex
+        style={{ width: "900px" }}
+        flexDirection="column"
+        marginTop="spacing3Xl"
+      >
+        <Stats />
+        <Members />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
